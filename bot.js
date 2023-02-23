56a936f54e024df713bb08a148884c90fdc5769e
Zubin <me@zubin.org.in>
2023-02-23 11:18:59 +0530

    Include authorized use

--- bot.js
+++ bot.js
@@ -1,9 +1,20 @@
 require("dotenv").config();
 const { Bot } = require("grammy");
+let BOT_DEVELOPER = 0 | process.env.BOT_DEVELOPER;
 
 // Bot
 
 const bot = new Bot(process.env.BOT_TOKEN);
+
+// Admin
+
+bot.use(async (ctx, next) => {
+  ctx.config = {
+    botDeveloper: BOT_DEVELOPER,
+    isDeveloper: ctx.from?.id === BOT_DEVELOPER,
+  };
+  await next();
+});
 
 // Commands
 
@@ -22,7 +33,7 @@ bot.command("start", async (ctx) => {
 bot.command("help", async (ctx) => {
   await ctx
     .reply(
-      "*@anzubo Project.*\n\n_This is a utility bot for @anzubo.\nUnauthorized access is not allowed.\nTo deploy your own:_",
+      "*@anzubo Project.*\n\n_This is a utility bot for @anzubo.\nUnauthorized access is not allowed.\nTo deploy your own:\nhttps://github.com/Grahtni/Channel-Sub-Telegram-Bot_",
       { parse_mode: "Markdown" }
     )
     .then(console.log("Help command sent to", ctx.from.id))
@@ -32,7 +43,14 @@ bot.command("help", async (ctx) => {
 // Messages
 
 bot.on("msg", async (ctx) => {
-  await ctx.reply("Works");
+  if (!ctx.config.isDeveloper) {
+    await ctx.reply(
+      "_This is a private utility bot for @anzubo. Unauthorized use is not allowed._",
+      { parse_mode: "Markdown" }
+    );
+  } else {
+    await ctx.reply("*Welcome admin ✨*", { parse_mode: "Markdown" });
+  }
 });
 
 // Run
